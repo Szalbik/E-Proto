4bf8c224b132ab6a2722acb1f55527ed4a88c950
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-04-17 13:49:25 +0200

    Add inject links to courses

--- src/main/java/com/resource/proto/Course.java
+++ src/main/java/com/resource/proto/Course.java
@@ -1,12 +1,29 @@
 package com.resource.proto;
 
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
+
+import javax.ws.rs.core.Link;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.List;
 
 @XmlRootElement
 public class Course {
     private long id;
     private String name;
     private String lecturer;
+    @InjectLinks({
+            @InjectLink(value = "/courses/{id}", rel = "self"),
+            @InjectLink(value = "/courses", rel = "parent"),
+//            @InjectLink(value = "/students/{index}", rel = "student")
+    })
+    @XmlElement(name = "link")
+    @XmlElementWrapper(name = "links")
+    @XmlJavaTypeAdapter(Link.JaxbAdapter.class)
+    private List<Link> links;
 
     public Course() {}
 
